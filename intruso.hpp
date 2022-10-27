5925f8072722ae2436ebecb2d440d73c82e37595
toninhobaianor <85882536+toninhobaianor@users.noreply.github.com>
2022-10-27 08:43:35 -0300

    Update intruso.hpp

--- intruso.hpp
+++ intruso.hpp
@@ -2,6 +2,8 @@
 
 class Intruso{
     /*Continue a implementação da classe Intruso*/
+    std::vector<std::string> ordem;
+    std::vector<std::map<char,char>> ordemnum;
 
     public:
         void set_senha_vazada(std::string vazou);
