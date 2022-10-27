c98d6df781cf1d31815ce3c29561af5c513c71ca
toninhobaianor <85882536+toninhobaianor@users.noreply.github.com>
2022-10-27 15:47:35 -0300

    Update intruso.cpp

--- intruso.cpp
+++ intruso.cpp
@@ -2,48 +2,58 @@
 #include <map>
 #include"intruso.hpp"
 
-void Intruso::set_senha_vazada(std::string senha){
- std::string ord;
- std::multimap<char,char> numOrd;
- for(int i = 0;i < 15;i++){
-     if(i > 9){
+ void Intruso::set_senha_vazada(std::string senha){
+
+    std::string ord;
+    std::multimap<char,char> numOrd;
+    for(int i = 0;i < 15;i++){
+      if(i > 9){
          ord[i - 10] = senha[i];
-     }
-     else{
+      }
+      else{
          if(i<2){
-            numOrd.insert(std::pair<char,char>('a',senha[i]));
+            numOrd.insert(std::pair<char,char>('A',senha[i]));
          }    
          if(i==2 || i==3){
-            numOrd.insert(std::pair<char,char>('b',senha[i]));
+            numOrd.insert(std::pair<char,char>('B',senha[i]));
          }
          if(i==4 || i==5){
-            numOrd.insert(std::pair<char,char>('c',senha[i]));
+            numOrd.insert(std::pair<char,char>('C',senha[i]));
          }
          if(i==6 || i==7){
-            numOrd.insert(std::pair<char,char>('d',senha[i]));
+            numOrd.insert(std::pair<char,char>('D',senha[i]));
          }
          if(i==8 || i==9){
-            numOrd.insert(std::pair<char,char>('e',senha[i]));
+            numOrd.insert(std::pair<char,char>('E',senha[i]));
          }
-     }
+      }
+    }
+    ordemletra.push_back(ord);
+    sequencia.push_back(numOrd);
  }
-   ordem.push_back(ord);
-   ordemnum.push_back(numOrd);
-}
 
 std::string Intruso::crack_senha(){
    std::string comp1;
    std::string comp2;
    std::multimap<char,char>::iterator it;
-   for(int i = 0;i < ordem.size() - 1;i++){
-      comp1 = ordem[i];
-      comp2 = ordem[i + 1];
-      for (it = numOrd.begin(); it!= numOrd.end(); it++){
+   for(int i = 0;i < ordemletra.size() - 1;i++){
+      comp1 = ordemletra[i];
+      comp2 = ordemletra[i + 1];
       for(int j = 0;j < 6; j++){
-         ordemnum[i];
-         ordenum[i+1];
+         for (it = sequencia[i].begin(); it!= sequencia[i].end(); it++){
+            if(comp1[j]==it->first){
+               
+            }
+            comp2[j];
+         }
       }
-   }
+      /*for (it = numOrd.begin(); it!= numOrd.end(); it++){
+      for(int j = 0;j < 6; j++){
+         sequencia[i];
+         sequencia[i+1];
+      }
+   }*/
+}
 }
 
 
