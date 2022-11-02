c3dc18fa99644a80507846087e0483fa6fc63ba2
toninhobaianor <85882536+toninhobaianor@users.noreply.github.com>
2022-11-02 11:54:49 -0300

    Update intruso.cpp

--- intruso.cpp
+++ intruso.cpp
@@ -5,25 +5,29 @@
 void Intruso::set_senha_vazada(std::string senha){
  std::string ord;
  std::multimap<char,char> numOrd;
- for(int i = 0;i < 15;i++){
-     if(i > 9){
-         ord[i - 10] = senha[i];
+ int j = 0;
+ for(int i = 0;i < 30;i++){
+     if(i > 20){
+         if(i % 2 == 0){
+            ord[j] = senha[i];
+            j++;
+         }
      }
      else{
-         if(i<2){
-            numOrd.insert(std::pair<char,char>('a',senha[i]));
+         if(i==0 || i==2){
+            numOrd.insert(std::pair<char,char>('A',senha[i]));
          }    
-         if(i==2 || i==3){
-            numOrd.insert(std::pair<char,char>('b',senha[i]));
+         if(i==4 || i==6){
+            numOrd.insert(std::pair<char,char>('B',senha[i]));
          }
-         if(i==4 || i==5){
-            numOrd.insert(std::pair<char,char>('c',senha[i]));
+         if(i==8 || i==10){
+            numOrd.insert(std::pair<char,char>('C',senha[i]));
          }
-         if(i==6 || i==7){
-            numOrd.insert(std::pair<char,char>('d',senha[i]));
+         if(i==12 || i==14){
+            numOrd.insert(std::pair<char,char>('D',senha[i]));
          }
-         if(i==8 || i==9){
-            numOrd.insert(std::pair<char,char>('e',senha[i]));
+         if(i==16 || i==18){
+            numOrd.insert(std::pair<char,char>('E',senha[i]));
          }
      }
  }
